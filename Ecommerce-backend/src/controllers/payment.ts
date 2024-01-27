37fcd42aacdb21f78b58fee329278af8de76af32
daljeet723 <dkaur5842@gmail.com>
2024-01-27 13:24:26 +0530

    Get all coupon and delete coupon by id

--- Ecommerce-backend/src/controllers/payment.ts
+++ Ecommerce-backend/src/controllers/payment.ts
@@ -1,18 +1,62 @@
+import { availableParallelism } from "os";
 import { TryCatch } from "../middlewares/error.js";
 import { Coupon } from "../models/coupon.js";
 import ErrorHandler from "../utils/utility-class.js";
+import { myCache } from "../app.js";
 
-export const newCoupon = TryCatch(async(req, res, next)=>{
-    const {coupon, amount} = req.body;
+export const newCoupon = TryCatch(async (req, res, next) => {
+    const { coupon, amount } = req.body;
 
-    if(!amount || !coupon){
-        return next(new ErrorHandler("Plesae provide both coupon code and amount",400));
+    if (!amount || !coupon) {
+        return next(new ErrorHandler("Plesae provide both coupon code and amount", 400));
     }
-    await Coupon.create({code: coupon, amount});
+    await Coupon.create({ code: coupon, amount });
 
     return res.status(201).json({
-        success:true, 
+        success: true,
         message: `Coupon ${coupon} created successfully with discount Rs ${amount}`
     })
 
-})
+});
+
+
+//GET DISCOUNT AMOUNT FROM CODE
+export const applyDiscount = TryCatch(async (req, res, next) => {
+    const { coupon } = req.query;
+
+    const discount = await Coupon.findOne({ code: coupon });
+
+    if (!discount) {
+        return next(new ErrorHandler("Invalid coupon code", 400));
+    }
+    return res.status(200).json({
+        success: true,
+        discount: discount.amount
+    })
+});
+
+export const allCoupons = TryCatch(async (req, res, next) => {
+
+    const coupons = await Coupon.find({});
+
+    return res.status(200).json({
+        success: true,
+        coupons
+    })
+});
+
+export const deleteCoupon = TryCatch(async (req, res, next) => {
+
+    const { id } = req.params
+    const coupon = await Coupon.findByIdAndDelete(id);
+    if (!coupon) {
+        return next(new ErrorHandler("Coupon does not exists.", 404));
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: `Coupon ${coupon.code} deleted successfully.`
+    })
+});
+
+
--- Ecommerce-backend/src/routes/payment.ts
+++ Ecommerce-backend/src/routes/payment.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import { isAdmin } from "../middlewares/auth.js";
-import { newCoupon } from "../controllers/payment.js";
+import { allCoupons, applyDiscount, deleteCoupon, newCoupon } from "../controllers/payment.js";
+import { isAscii } from "buffer";
 
 const app = express.Router();
 
 //ROUTE: /api/v1/payment/coupon/new
-app.post("/coupon/new", newCoupon)
+app.post("/coupon/new", isAdmin, newCoupon);
+
+//ROUTE: /api/v1/payment/discount   
+app.get("/discount", applyDiscount);
+
+//ROUTE: /api/v1/payment/coupon/all
+app.get("/coupon/all", isAdmin, allCoupons);
+
+//ROUTE: /api/v1/payment/coupon/id
+app.delete("/coupon/:id", isAdmin, deleteCoupon);
+
 export default app;
