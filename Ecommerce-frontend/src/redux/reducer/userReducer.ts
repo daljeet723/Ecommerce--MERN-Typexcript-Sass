251085c087c603aab2dbb91e41114e52f97733c3
daljeet723 <dkaur5842@gmail.com>
2024-02-17 16:58:20 +0530

    Create user reducer

---
+++ Ecommerce-frontend/src/redux/reducer/userReducer.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {User} from "../../types/types"
+import { UserReducerInitialState } from "../../types/reducer-types";
+
+const initialState : UserReducerInitialState={
+    user: null,
+    loading: true
+}
+
+export const userReducer =createSlice({
+    name:"userReducer",
+    initialState,
+    reducers:{
+        userExist: (state, action:PayloadAction<User>)=>{
+            state.loading=false,
+            state.user = action.payload
+        },
+        userNotExist: (state)=>{
+            state.loading = true,
+            state.user = null
+        }
+    }
+
+})
